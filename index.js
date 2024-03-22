ffd11a6fe1456d21be2606822c847f1e0bc33814
dibkb <dibas9110@gmail.com>
2024-03-22 20:58:34 +0530

    extract links

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-"master branch";
+const fs = require("fs");
+const puppeteer = require("puppeteer");
+async function run() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  try {
+    await page.goto(
+      "http://www.howstat.com/cricket/Statistics/IPL/SeriesMatches.asp?s=2023"
+    );
+    const links = await page.evaluate(() => {
+      const linkTable = document.querySelectorAll(".LinkTable"); // Select all elements with class name 'LinkTable'
+      return Array.from(linkTable).map((link) => link.href); // Extract href attribute from each link
+    });
+  } catch (error) {
+    console.error("error", error.message);
+  } finally {
+    await browser.close();
+  }
+}
+run();
