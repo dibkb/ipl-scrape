ff2d674cdb5a255618767d96ac2c9140a873194a
dibkb <dibas9110@gmail.com>
2024-03-23 00:57:30 +0530

    pusj

--- index.js
+++ index.js
@@ -5,12 +5,39 @@ async function run() {
   const page = await browser.newPage();
   try {
     await page.goto(
-      "http://www.howstat.com/cricket/Statistics/IPL/SeriesMatches.asp?s=2023"
+      "http://www.howstat.com/cricket/Statistics/IPL/MatchScorecard.asp?MatchCode=0959"
     );
-    const links = await page.evaluate(() => {
-      const linkTable = document.querySelectorAll(".LinkTable"); // Select all elements with class name 'LinkTable'
-      return Array.from(linkTable).map((link) => link.href); // Extract href attribute from each link
-    });
+    // Select all tr elements
+    const trElements = await page.$$("tr");
+    const tdTexts = [];
+    const teams = [];
+    for (const tr of trElements) {
+      const divwrap = await tr.$("td.ScorecardCountry3");
+      if (divwrap) {
+        const team = await (await divwrap.getProperty("innerText")).jsonValue();
+        teams.push(team);
+      }
+      // player element
+      const aElement = await tr.$("a.ScorecardLink1");
+      if (aElement) {
+        const player = await (
+          await aElement.getProperty("innerText")
+        ).jsonValue();
+        // SKIP if season
+        if (player.startsWith("Season") || player.startsWith("BOWLING"));
+        const tdElements = await tr.$$("td");
+        const tdInnerTexts = [];
+        const tdTexts = [];
+        for (const td of tdElements) {
+          const tdInnerText = await (
+            await td.getProperty("innerText")
+          ).jsonValue();
+          tdInnerTexts.push(tdInnerText.trim());
+        }
+        console.log(tdTexts);
+      }
+      console.log(tdTexts);
+    }
   } catch (error) {
     console.error("error", error.message);
   } finally {
